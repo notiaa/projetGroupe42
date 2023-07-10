de7a728da7804b3470abbfb03707fd51a446357d
Tony Fitahiana <121194228+fitahianatony@users.noreply.github.com>
2023-07-10 15:42:53 +0300

    Add files via upload

--- application/models/regime_model.php
+++ application/models/regime_model.php
@@ -4,12 +4,48 @@
         public function getDetail($idRegime){
             $sql = "SELECT dr.idRegime, c.nom, c.description, dr.quantite, u.value, '1 jour' as duree from detail_regime as dr JOIN composant as c ON dr.idcomposant=c.idcomposant JOIN unite as u ON c.idUnite=u.idUnite WHERE dr.idregime=%s";
             $sql = sprintf($sql, $idRegime);
-            $rows = $this->db->query($sql);
-            return $rows->result_array();
+            //$rows = $this->db->query($sql);
+            //return $rows->result_array();
+            echo $sql;
         }
 
         public function getRegimeFit($gain, $height, $weight){
+            $ans = array();
+            if($gain){
+                $sql = "SELECT * FROM regime WHERE coefficient > 0";
+                $result = $this->db->query($sql);
+                foreach ($result->result_array() as $row) {
+                    $duree = round(10/((($height - 100)/ $weight) * $row['coefficient']));
+                    $values = array(
+                        'idRegime' => $row['idRegime'], 
+                        'duree' => $duree,
+                        'prix' => $row['prix'] * $duree
+                        );
+                    array_push($ans, $values);
+                }
+            }
+            else{
+                $sql = "SELECT * FROM regime WHERE coefficient < 0";
+                $result = $this->db->query($sql);
+                foreach ($result->result_array() as $row) {
+                    $duree = round(10/((($height - 100)/ $weight) * -$row['coefficient']));
+                    $values = array(
+                        'idRegime' => $row['idRegime'], 
+                        'duree' => $duree,
+                        'prix' => $row['prix'] * $duree
+                        );
+                    array_push($ans, $values);
+                }
+            }
+            return $ans;
+            // $coef = ($height - 100)/$weight;
+            // return 10 / ($coef * 1);    
+        }
 
+        public function getComposant(){
+            $sql = "SELECT * FROM composant";
+            $result = $this->db->query($sql);
+            return $result->result_array();
         }
     }
 ?>
