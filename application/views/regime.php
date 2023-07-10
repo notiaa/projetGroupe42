6c6053ac5c7559d2639132c2bca0731f8c527433
AnjaArisoa <119120342+AnjaArisoa@users.noreply.github.com>
2023-07-10 16:27:18 +0300

    frontduree

--- application/views/regime.php
+++ application/views/regime.php
@@ -7,44 +7,64 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
     <title>Document</title>
     <style>
-        .image-container {
-            text-align: center;
-            margin-bottom: 30px;
-            margin-top: 30px;
-           
-        }
-        .image-container img {
-            width: 100%;
-            max-width: 200px;
-        }
-        .image-container label {
-            font-family: sans-serif;
-        }
+       .content{
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+}
+
+.boite{
+    background-color: aqua;
+    height: 200px;
+    width: 300px;
+    margin-top: 200px;
+    border-radius: 10px;
+    text-align:center;
+    box-shadow: 10px 10px 10px rgba(0,0,0,0.2);
+
+}
+a{
+    text-decoration:none;
+    color:black;
+    font-size:30px;
+    font-family: sans-serif;
+}
+h4{
+    padding: 0;
+}
+.info{
+    text-align: center;
+    padding-left: 5px;
+    font-size: 12px;
+    font-family: sans-serif;
+   
+    
+}
+img{
+    width: 300px;
+    height: 200px;
+}
+
+
     </style>
 </head>
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-                <div class="image-container">
-                    <img src="./assets/img/icon.png" class="img-fluid" alt="...">
-                    <div>
-                        <span class="ti-timer"><span style="margin-top:20px;font-family:sans-serif"> Données de l'image 1</span></span></p>
-                        <span class="ti-apple"><span style="margin-top:20px;font-family:sans-serif"> Données de l'image 1</span></span></p>
-                    </div>
-                </div>
-               
-            </div>
-            <div class="col-md-6">
-                <div class="image-container">
-                    <img src="./assets/img/icon.png" class="img-fluid" alt="...">
-                    <div>
-                        <span class="ti-basketball"><span style="margin-top:20px;font-family:sans-serif"> Données de l'image 1</span></span></p>
-                        <span class="ti-timer"><span style="margin-top:20px;font-family:sans-serif"> Données de l'image 1</span></span></p>
-                    </div>
-                </div>
-            </div>
+<div class="content">
+    <div class="boite">
+            <img src="<?php echo base_url().'assets/img/icon.png'?>" alt="">
+            
         </div>
+        <div class="info">si vous n'aimez pas trop courir,ou que vous n'avez pas vraiemnt de cardio</div>
+        <div class="boite">
+        <img src="<?php echo base_url().'assets/img/icon.png'?>" alt="">
+            
+        </div>
+        <div class="info">si vous n'aimez pas trop courir,ou que vous n'avez pas vraiemnt de cardio</div>
+        <div class="boite">
+        <img src="<?php echo base_url().'assets/img/icon.png'?>" alt="">
+           
+        </div>
+        <div class="info">si vous n'aimez pas trop courir,ou que vous n'avez pas vraiemnt de cardio</div>
     </div>
     <a class="btn " href="<?php echo site_url('Regime/pdf');?>" style="background-color:#666699;color:white;font-family:sans-serif;margin-left:1100px;margin-top:200px">Exporter</a>
     
